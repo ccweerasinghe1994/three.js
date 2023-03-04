26c8e97360a92245e6b4637b94ebabe7630989d7
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-03-04 11:59:33 +0530

    feat: iniitial commit

---
+++ lession-3/script.js
@@ -0,0 +1,35 @@
+// scene
+const scene = new THREE.Scene();
+
+// Red Cube
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+});
+
+ const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// camera
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+scene.add(camera);
+
+// renderer
+const canvas = document.querySelector(".webgl");
+const render = new THREE.WebGLRenderer({
+  canvas,
+});
+
+render.setSize(sizes.width, sizes.height);
+
+render.render(scene, camera);
